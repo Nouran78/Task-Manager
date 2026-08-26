df743380510244b59ae7e1f98b7c51dce4096b23
nouran essam <nouranessam782@gmail.com>
2026-08-26 16:24:07 +0300

    Initial project setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express= require ("express");
+const dotenv=require ("dotenv");
+const connectDB= require("./config/db");
+dotenv.config();
+const app= express();
+app.use(express.json());
+
+connectDB();
+
+app.get("/",(req,res)=> {
+    res.json ({
+message:"Api is working"
+
+    });
+
+});
+
+const PORT=process.env.PORT || 5000;
+app.listen (PORT, ()=> {
+    console.log (`Server is running on this ${PORT}`);
+}) ;
